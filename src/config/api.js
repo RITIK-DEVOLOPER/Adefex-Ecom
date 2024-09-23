ea995244297fe3eef9970743640c9e13fa43f915
Ritik Jain <ritik.jain.3444@gmail.com>
2024-09-23 10:14:01 +0530

    Deployed on Railway

--- src/config/api.js
+++ src/config/api.js
@@ -1,18 +1,17 @@
+import axios from "axios";
+const DEPLOYED = "adefex-ecomm-backend-production.up.railway.app";
+// const LOCALHOST = "http://localhost:5454";
 
-import axios from 'axios';
-// const DEPLOYED='adefex-ecomm-backend.railway.internal'
-const LOCALHOST='http://localhost:5454'
-
-export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL = DEPLOYED;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-const token = localStorage.getItem('jwt');
+const token = localStorage.getItem("jwt");
 
-api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.post["Content-Type"] = "application/json";
 
 export default api;
